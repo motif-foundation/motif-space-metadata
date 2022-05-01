8702da34b0d874b0bb1f9a6fd59762db0f21a0d2
Steve <johnw@motif.foundation>
2022-05-01 22:03:59 +0300

    update

--- types/motif/MotifSpace20210905.d.ts
+++ types/motif/MotifSpace20210905.d.ts
@@ -8,34 +8,13 @@
 /**
  * This schema describes version 20210905 of the Motif Metadata Standard
  */
-export interface MotifSpace20210905 {
-  /**
-   * Type of the NFT Item
-   */
-  spaceType: string;
-  /**
-   * Owned NFT item
-   */
-  blockchain: string;
-  /**
-   * Name of the NFT Item
-   */
-  name: string;
-  /**
-   * Description of the NFT Item
-   */
-  description: string;
-  /**
-   * MimeType of NFT Item
-   */
-  mimeType: string;
-
-  code: string;
-  data: string;
-  /**
-   * Blockchain of the NFT item under the item type
-   */
-
+export interface MotifSpace20210905 { 
+  spaceType: string; 
+  blockchain: string; 
+  name: string; 
+  description: string; 
+  mimeType: string; 
+  data: string; 
   access: string;
   version: string;
 }
